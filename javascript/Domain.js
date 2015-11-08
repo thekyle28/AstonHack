6dcaccd4886e218e1d4c4e1ec0dfb460e05063f1
rrama <ben_durrans@hotmail.co.uk>
2015-11-08 01:59:58 +0000

    Domains holdable as objects.

---
+++ javascript/Domain.js
@@ -0,0 +1,10 @@
+function Domain(name, json) {
+
+    this.name = name;
+    this.topics = {};
+
+    var data = json.Topics.Data;
+    for (var i = data.length - 1; i >= 0; i--) {
+        this.topics[data[i].Topic.split("/")[0]] = data[i].TopicalTrustFlow;
+    }
+}
