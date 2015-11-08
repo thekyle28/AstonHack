607a166f5bfbb40e5f785f7a64912915aa46bf70
rrama <ben_durrans@hotmail.co.uk>
2015-11-08 01:14:45 +0000

    Added calls to fake locally saved data. Fixed html file headders.

--- javascript/Calls.js
+++ javascript/Calls.js
@@ -3,6 +3,8 @@ function Calls() {
     var useFake = true;
 
     this.makeCall = function (cmd, params) {
+        if (useFake)
+            return this.fakeCall(cmd, params);
         var ajex = $.ajax({
             url: "../php/call.php",
             cmd: cmd,
@@ -13,5 +15,22 @@ function Calls() {
         var json = $.parseJSON(ajex.responseText);
         return json.DataTables;
     };
+
+    this.fakeCall = function (cmd, params) {
+        var file = "";
+        switch (cmd) {
+            case "GetTopics":
+                file = params.substr(5);
+                console.log(file);
+                break;
+        }
+        var ajex = $.ajax({
+            url: ("../json/" + file + ".json"),
+            dataType: "json",
+            async: false
+        });
+        var json = $.parseJSON(ajex.responseText);
+        return json.DataTables;
+    }
 
 }
