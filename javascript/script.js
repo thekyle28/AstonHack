0d9a5e9c8ab76cfb7ee07abe41226d1593200695
rrama <ben_durrans@hotmail.co.uk>
2015-11-08 01:34:40 +0000

    Stopwatch moved.

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,5 @@
+/* global Stopwatch */
+
 var counter = 0;
 var timer;
 $( initialise );
@@ -7,11 +9,11 @@ var sites = [{name: "Google", health: 10},
          {name:"Reddit", health: 4}];
 
 function initialise(){
-    timer  = new Stopwatch(document.getElementById("timer"));
+    timer = new Stopwatch(document.getElementById("timer"));
     shuffle(sites);
     createTarget();
     timer.start();
-};
+}
 
 
 function createTarget(){
@@ -64,72 +66,3 @@ function shuffle(array) {
 
   return array;
 }
-
-var Stopwatch = function(elem, options) {
-    
-    var timer = createTimer(),
-        offset,
-        clock,
-        interval;
-
-    // default options
-    options = options || {};
-    options.delay = options.delay || 1;
-
-    // append elements     
-    elem.appendChild(timer);
-
-    // initialize
-    reset();
-
-    // private functions
-    function createTimer() {
-        return document.createElement("span");
-    }
-
-    function start() {
-        if (!interval) {
-            offset = Date.now();
-            interval = setInterval(update, options.delay);
-        }
-    }
-
-    function stop() {
-        if (interval) {
-            clearInterval(interval);
-            interval = null;
-        }
-    }
-
-    function reset() {
-        clock = 0;
-        render();
-    }
-    
-    function time() {
-        return clock;
-    }
-
-    function update() {
-        clock += delta();
-        render();
-    }
-
-    function render() {
-        timer.innerHTML = (clock / 1000).toFixed(2) + "s";
-    }
-
-    function delta() {
-        var now = Date.now(),
-            d = now - offset;
-
-        offset = now;
-        return d;
-    }
-
-    // public API
-    this.time = time;
-    this.start = start;
-    this.stop = stop;
-    this.reset = reset;
-};
